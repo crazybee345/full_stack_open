726c20572bbd9038218e96a36959b07683825280
crazybee345 <149293467+crazybee345@users.noreply.github.com>
2024-01-06 13:45:23 +0200

    Add files via upload

---
+++ part2/phonebook/src/components/Persons.jsx
@@ -0,0 +1,14 @@
+const Persons = ({ persons,  handleDeletePerson}) => {
+  return (
+    <ul>
+      {persons.map((person) => (
+        <li key={person.id}>
+          {person.name} {person.number}
+          <button onClick={() => handleDeletePerson(person.id)}>delete</button>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default Persons
