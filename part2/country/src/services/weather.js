5f1b9fa01388b5100193d2e8d4d2e7f61127af5f
crazybee345 <149293467+crazybee345@users.noreply.github.com>
2024-01-06 13:49:39 +0200

    Delete part2 directory

--- part2/country/src/services/weather.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q='
-
-const getWeather = (capital, api_key) => {
-    const request = axios.get(`${weatherUrl}${capital}&appid=${api_key}`)
-    return request.then(response => response.data)
-}
-
-export default { getWeather }
